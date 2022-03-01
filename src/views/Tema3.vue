4601ccd4decb0d2dca554f14c0fbcf550a2f9fc8
Edgar Mauricio Cortes <edgarm.cortes@misena.edu.co>
2022-03-01 16:09:54 -0500

    tema 3 y Sintesis

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -29,18 +29,129 @@
 
     #t3_1.titulo-segundo.color-acento-contenido
       h2 3.1 Tradiciones
+
+    p.mb-5 Los pueblos indígenas tienen sus propias tradiciones, la cual es una manifestación popular, un tanto autónoma, voluntaria y espontánea. Para conocer precisamente sobre este tema, es importante el conocimiento de estas, como se detalla a continuación:
+        
+    .tema-1
+        TabsA.color-acento-botones
+          .tarjeta.color-acento-contenido.p-5(titulo="Cultura médica tradicional.")
+            h4 Cultura médica tradicional.
+            p.mb-0 De acuerdo al MSPS, la cultura medica tradicional se puede definir como el conjunto de conocimientos, creencias, valores, actitudes y comportamientos que se acostumbran ancestralmente en nuestras comunidades, para la prevención, el diagnóstico, el tratamiento y la explicación de las enfermedades. (Ministerio de Salud y Protección Social, 1992) En este sentido, se destaca la Resolución 5078 de 1992 en la cual establece que el Ministerio de Salud, promoverá y facilitará el intercambio de conocimientos entre los agentes de las Culturas Médicas Tradicionales, el personal institucional de salud y de los Centros de Educación formal e informal y promoverá la articulación al sistema de prestación servicios de salud de aquellos recursos y las Culturas Médicas Tradicionales y las Terapéuticas alternativas, que en cada región resulten como consecuencia de acuerdos entre las autoridades de salud y los representantes de las comunidades, y además que, las instituciones prestatarias de servicios de salud, podrán vincular profesionales con formación en las culturas Médicas Tradicionales y Terapéuticas alternativas, buscando la complementación de los distintos saberes (Ministerio de Salud y Protección Social, 1992)            
+          .tarjeta.color-acento-contenido.p-5(titulo="Cosmovisión y prácticas ancestrales.")
+            h4 Cosmovisión y prácticas ancestrales.
+            p.mb-5 La cosmovisión se centra en las percepciones, conceptuaciones y valoraciones de los pueblos indígenas, así mismo, las prácticas ancestrales hacen referencia a los conocimientos y prácticas desarrolladas por las comunidades locales a través del tiempo para comprender y manejar sus propios ambientes locales, entre las más utilizadas, se encuentran: 
+            .tema-3
+              ol.lista-ol--cuadro.mb-0
+                li 
+                  .lista-ol--cuadro__vineta
+                    span.text-white 1
+                  | Confiesos.
+                li 
+                  .lista-ol--cuadro__vineta
+                    span.text-white 2
+                  | Aseguranzas.
+                li 
+                  .lista-ol--cuadro__vineta
+                    span.text-white 3
+                  | Adivinanzas.
+                li 
+                  .lista-ol--cuadro__vineta
+                    span.text-white 4
+                  | Tumas o piedras.
+                li 
+                  .lista-ol--cuadro__vineta
+                    span.text-white 5
+                  | Cantos6 y bailes.
+                li 
+                  .lista-ol--cuadro__vineta
+                    span.text-white 6
+                  | Partería indígena.
+                li 
+                  .lista-ol--cuadro__vineta
+                    span.text-white 7
+                  | Chamanismo.
+                li 
+                  .lista-ol--cuadro__vineta
+                    spa.text-whiten 8
+                  | Espiritualidad.
+                li 
+                  .lista-ol--cuadro__vineta
+                    span.text-white 9
+                  | Sobandería, partería y yerbatería.
+                li 
+                  .lista-ol--cuadro__vineta
+                    span.text-white 10
+                  | Médicos tradicionales.
+                li 
+                  .lista-ol--cuadro__vineta
+                    span.text-white 11
+                  | El curandero.
+
     Separador
 
     #t3_2.titulo-segundo.color-acento-contenido
       h2 3.2 Salud intercultural en pueblos indígenas
+
+    .row.align-items-center.justify-content-center.mb-5
+      .col-lg-6.d-flex.align-items-center
+        p.mb-0 La salud intercultural en los pueblos indígenas debe partir del respeto por parte de los equipos de salud occidental, a la medicina tradicional, desde el concepto de salud en la cosmovisión indígena, su concepto de enfermedad como un desequilibrio (ni limitado solamente a lo biológico, sino también en su colectivo, en lo espiritual y con el equilibrio con la naturaleza) del respeto por sus autoridades y médicos tradicionales y sobre el conocimiento ancestral en su seguridad alimentaria, en sus prácticas médicas ancestrales, su conocimiento del uso de las plantas y ritos para el mantenimiento y recuperación de la salud individual y colectiva.
+      .col-lg-6.d-flex.align-items-center
+        figure.mb-0
+          img(src="@/assets/curso/tema3/img02.png")  
+
+    .tema-2.mb-5
+      .cajon.color-secundario.p-5
+        p.mb-0 La salud en un enfoque intercultural implica la complementariedad de los saberes, mediante el diálogo de saberes en igualdad de condiciones, incorporar el saber de los médicos tradicionales y complementarnos con ellos en la atención de la salud para las personas y las comunidades.
+
+    p.mb-5 La recuperación y conservación del conocimiento cultural debe ser integral con la participación de los pueblos involucrados y ligada a la preservación de la diversidad biológica. La protección y el cultivo de las especies en peligro de extinción deben formar parte de las políticas nacionales. El conocimiento sistemático del saber indígena, el intercambio local, nacional y regional de la información es esencial como un medio para sensibilizar y educar a la sociedad civil acerca de las culturas que integran el país (OPS/OMS, 1996 Ref:23) (Organización Panamericana de la Salud, 1997) En este sentido, la Iniciativa de Salud de los Pueblos Indígenas, en el marco de los principios de Winnipeg, aboga por el abordaje integral de la salud y el respeto y revitalización de la cultura de los pueblos indígenas de la región en un clima de mutuo respeto, participación sistemática y reciprocidad en las relaciones.
+
+    .tema-2.mb-5
+      .cajon.color-secundario.p-5
+        p.mb-0 El desafío que presenta la carencia de conocimientos e información adecuados y suficientes sobre la salud de los pueblos indígenas y sobre experiencias en la complementariedad de los sistemas médicos tradicionales y occidentales, frente al imperativo de actuar y lograr impactos inmediatos, exige el diseño de estrategias que permitan tanto, generar conocimientos e información adecuada mientras se actúa (“aprender haciendo”), cómo rescatar de manera sistemática el conocimiento y la información generados por la experiencia (“aprender de lo que se ha hecho y de lo que se está haciendo”). Los conocimientos generados, deberán convertirse en las pautas que orienten el trabajo con pueblos indígenas y de quienes se comprometan con el mismo (Organización Panamericana de la Salud, 1997).
+
+
+
     Separador
 
     #t3_3.titulo-segundo.color-acento-contenido
       h2 3.3 Análisis de determinantes sociales de salud
+    p.mb-5 La población y los pueblos indígenas se desenvuelven en un contexto internacional, nacional, comunitario y familiar, esto nos aproxima a los determinantes que actúan en sus condiciones de vida y estado de salud, a los factores protectores, a los factores de riesgo, al perfil epidemiológico y a las estrategias de abordaje de su salud. Partiendo de lo anterior, en el siguiente video podrá conocer un análisis detallado sobre estos determinantes: 
+
+    div.px-5
+      figure
+        .video
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     Separador
 
     #t3_4.titulo-segundo.color-acento-contenido
       h2 3.4 Medicina tradicional y equipos de salud indígena
+
+    p.mb-5 La caracterización de los diferentes grupos poblacionales, la comprensión de los sistemas de creencias y valores que determinan los procesos de mantenimiento y restauración de la salud y el aprovechamiento de los recursos comunitarios, promueve el enfoque intercultural integral de la salud a partir de la armonización de los sistemas de salud tradicionales y el sistema de salud convencional (Organización Panamericana de la Salud, 2006). Por esto, el abordaje de la salud desde un enfoque centrado en la interculturalidad posibilita el reconocimiento de recursos humanos, terapéuticos y estrategias que exceden los paradigmas del sistema de salud convencional, ya que también se generan estrategias de curación prevención, rehabilitación y promoción de la salud, como en el sistema de salud convencional. 
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-6
+        .tema-2
+          .bloque-texto-b.color-secundario.p-4
+            .bloque-texto-b__texto
+              i.fas.fa-quote-left
+              h4.mb-0 Hay enfermedades que no cura el médico y que cura el shapori, hay enfermedades que no cura el shapori y el médico si cura, hay enfermedades que no curan ni el médico, ni el shapori
+              i.fas.fa-quote-right
+      .col-lg-6
+        figure.mb-0
+          img(src="@/assets/curso/tema3/img03.png")  
+
+    .tema-2.mb-5
+      .cajon.color-secundario.p-5
+        p.mb-0 El espacio de la enfermedad no es el espacio del shamán, es apenas una parte de su espacio que incluye la relación con el cosmos, con el orden natural y social de su comunidad (Organización Panamericana de la Salud, 2006)
+
+
+    .row.justify-content-center.align-items-center
+      .col-lg-6
+        figure.mb-0
+          img(src="@/assets/curso/tema3/img04.png")  
+      .col-lg-6
+        p.mb-0 De acuerdo con lo anterior, la medicina tradicional comprende el conjunto de ideas, conceptos, creencias, mitos y procedimientos, sean explicables o no, relativos a las enfermedades físicas, mentales o desequilibrios sociales en un pueblo determinado. En este sentido, se define como el conjunto de conocimientos explica la etiología, la nosología y los procedimientos de diagnóstico, pronóstico, curación y prevención de las enfermedades. 
+      
     Separador
    
 </template>
